7a7368a502245ec6a731634b2e9ba959b4bf4f05
storeetsy72-lgtm <storeetsy72@gmail.com>
2026-09-04 23:13:37 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  nitro: process.env.CF_PAGES ? {
-    preset: "cloudflare-pages",
-    output: {
-      dir: "dist",
-      publicDir: "dist/client",
-    }
-  } : {},
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  nitro: process.env.CF_PAGES
+    ? {
+        preset: "cloudflare-pages",
+        output: {
+          dir: "dist",
+          publicDir: "dist/client",
+        },
+      }
+    : {},
   tanstackStart: {
     server: { entry: "server" },
   },
